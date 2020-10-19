26cc6ad34dc577104f64afcec010a93682083f5c
Jay Atalykov <atalykovj@gmail.com>
2020-10-19 22:30:44 +0300

    Updates

---
+++ precache-manifest.ea892ec3454cbf93e4fb111fa6c510d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7503ace3a9d17aec9b7a76f7c18b110e",
+    "url": "/expenseTracker/index.html"
+  },
+  {
+    "revision": "4062915e0c8abb6affa7",
+    "url": "/expenseTracker/static/css/main.b975309a.chunk.css"
+  },
+  {
+    "revision": "1762b42d8fca8115944d",
+    "url": "/expenseTracker/static/js/2.d0238ad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expenseTracker/static/js/2.d0238ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4062915e0c8abb6affa7",
+    "url": "/expenseTracker/static/js/main.db1e0235.chunk.js"
+  },
+  {
+    "revision": "280d161a2221fc82b678",
+    "url": "/expenseTracker/static/js/runtime-main.4562fb1d.js"
+  }
+]);
