83aae2ef869f89e13080869d36f91a1a8dd65275
Jay Atalykov <atalykovj@gmail.com>
2020-10-19 22:18:05 +0300

    bug fixing
    fixed some style bugs. made form filling required

--- src/components/form.js
+++ src/components/form.js
@@ -18,8 +18,8 @@ function Form({toggle, handleToggle, handleSubmit}) {
     return(
         <form className={toggle?"":"remove-form"} onSubmit={onSubmit}>
             <div className="form">
-                <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder="Title..."/>
-                <input type="number" value={newAmount} onChange={(e) => setNewAmount(e.target.value)} placeholder="Amount..."/>
+                <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder="Title..." required={true} />
+                <input type="number" value={newAmount} onChange={(e) => setNewAmount(e.target.value)} placeholder="Amount..." required={true} />
             </div>
             <button type="submit">Submit</button>
             <button className="remove" type="button" onClick={handleToggle}>x</button>
